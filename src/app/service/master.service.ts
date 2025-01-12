73d663e5dbd29b1047b034909762637d40f12561
Arnob Mahmud <arnob_t78@yahoo.com>
2025-01-12 17:15:26 +0600

    Trying to fix issue with vercel deployment

--- src/app/service/master.service.ts
+++ src/app/service/master.service.ts
@@ -1,3 +1,90 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+// import {
+//   IApiResponse,
+//   IProject,
+//   IProjectEmployee,
+// } from '../model/interface/master';
+// import { Employee } from '../model/class/Employee';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class MasterService {
+//   apiUrl: string = '/api/EmployeeManagement/';
+
+//   constructor(private http: HttpClient) {}
+
+//   getAllDept(): Observable<IApiResponse> {
+//     return this.http.get<IApiResponse>(this.apiUrl + 'GetParentDepartment');
+//   }
+
+//   getChildDeptById(deptId: number): Observable<IApiResponse> {
+//     return this.http.get<IApiResponse>(
+//       `${this.apiUrl}GetChildDepartmentByParentId?deptId=${deptId}`
+//     );
+//   }
+
+//   saveEmp(obj: Employee): Observable<IApiResponse> {
+//     return this.http.post<IApiResponse>(this.apiUrl + 'CreateEmployee', obj);
+//   }
+
+//   getAllEmp(): Observable<Employee[]> {
+//     return this.http.get<Employee[]>(this.apiUrl + 'GetAllEmployees');
+//   }
+
+//   updateEmp(obj: Employee): Observable<IApiResponse> {
+//     return this.http.put<IApiResponse>(
+//       this.apiUrl + 'UpdateEmployee/' + obj.employeeId,
+//       obj
+//     );
+//   }
+
+//   deleteEmpById(id: number): Observable<IApiResponse> {
+//     return this.http.delete<IApiResponse>(this.apiUrl + 'DeleteEmployee/' + id);
+//   }
+
+//   saveProject(obj: Employee): Observable<IProject> {
+//     return this.http.post<IProject>(this.apiUrl + 'CreateProject', obj);
+//   }
+
+//   updateProject(obj: IProject): Observable<IProject> {
+//     return this.http.put<IProject>(
+//       this.apiUrl + 'UpdateProject/' + obj.projectId,
+//       obj
+//     );
+//   }
+
+//   getAllProjects(): Observable<IProject[]> {
+//     return this.http.get<IProject[]>(this.apiUrl + 'GetAllProjects');
+//   }
+
+//   getProjectById(id: number): Observable<IProject> {
+//     return this.http.get<IProject>(this.apiUrl + 'GetProject/' + id);
+//   }
+
+//   getProjectEmp(): Observable<IProjectEmployee[]> {
+//     return this.http.get<IProjectEmployee[]>(
+//       this.apiUrl + 'GetAllProjectEmployees'
+//     );
+//   }
+
+//   saveProjectEmp(obj: IProjectEmployee): Observable<IProject> {
+//     return this.http.post<IProject>(this.apiUrl + 'CreateProjectEmployee', obj);
+//   }
+
+//   updateProjectEmp(obj: IProjectEmployee): Observable<IProjectEmployee> {
+//     return this.http.put<IProjectEmployee>(
+//       this.apiUrl + 'UpdateProjectEmployee/' + obj.empProjectId,
+//       obj
+//     );
+//   }
+
+//   getDashboardData(): Observable<any> {
+//     return this.http.get<any>(this.apiUrl + 'GetDashboard');
+//   }
+// }
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -12,7 +99,8 @@ import { Employee } from '../model/class/Employee';
   providedIn: 'root',
 })
 export class MasterService {
-  apiUrl: string = '/api/EmployeeManagement/';
+  // Use the full API URL directly
+  apiUrl: string = 'https://projectapi.gerasim.in/api/EmployeeManagement/';
 
   constructor(private http: HttpClient) {}
 
@@ -20,9 +108,9 @@ export class MasterService {
     return this.http.get<IApiResponse>(this.apiUrl + 'GetParentDepartment');
   }
 
-  getChildDeptById(deptId: number): Observable<IApiResponse> {
+  getChildDeptById(deptid: number): Observable<IApiResponse> {
     return this.http.get<IApiResponse>(
-      `${this.apiUrl}GetChildDepartmentByParentId?deptId=${deptId}`
+      `${this.apiUrl}GetChildDepartmentByParentId?deptId=${deptid}`
     );
   }
 
