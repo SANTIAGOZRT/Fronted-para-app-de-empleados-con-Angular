fcb0eb18a19e45b7975665301fe88464f8225947
Arnob Mahmud <arnob_t78@yahoo.com>
2025-01-12 17:24:53 +0600

    Fixing the issue for vercel deployment

--- src/app/service/master.service.ts
+++ src/app/service/master.service.ts
@@ -99,77 +99,102 @@ import { Employee } from '../model/class/Employee';
   providedIn: 'root',
 })
 export class MasterService {
-  // Use the full API URL directly
+  // Use a CORS proxy
+  corsProxyUrl: string = 'https://cors-anywhere.herokuapp.com/';
   apiUrl: string = 'https://projectapi.gerasim.in/api/EmployeeManagement/';
 
   constructor(private http: HttpClient) {}
 
   getAllDept(): Observable<IApiResponse> {
-    return this.http.get<IApiResponse>(this.apiUrl + 'GetParentDepartment');
+    return this.http.get<IApiResponse>(
+      this.corsProxyUrl + this.apiUrl + 'GetParentDepartment'
+    );
   }
 
   getChildDeptById(deptid: number): Observable<IApiResponse> {
     return this.http.get<IApiResponse>(
-      `${this.apiUrl}GetChildDepartmentByParentId?deptId=${deptid}`
+      `${
+        this.corsProxyUrl + this.apiUrl
+      }GetChildDepartmentByParentId?deptId=${deptid}`
     );
   }
 
   saveEmp(obj: Employee): Observable<IApiResponse> {
-    return this.http.post<IApiResponse>(this.apiUrl + 'CreateEmployee', obj);
+    return this.http.post<IApiResponse>(
+      this.corsProxyUrl + this.apiUrl + 'CreateEmployee',
+      obj
+    );
   }
 
   getAllEmp(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(this.apiUrl + 'GetAllEmployees');
+    return this.http.get<Employee[]>(
+      this.corsProxyUrl + this.apiUrl + 'GetAllEmployees'
+    );
   }
 
   updateEmp(obj: Employee): Observable<IApiResponse> {
     return this.http.put<IApiResponse>(
-      this.apiUrl + 'UpdateEmployee/' + obj.employeeId,
+      this.corsProxyUrl + this.apiUrl + 'UpdateEmployee/' + obj.employeeId,
       obj
     );
   }
 
   deleteEmpById(id: number): Observable<IApiResponse> {
-    return this.http.delete<IApiResponse>(this.apiUrl + 'DeleteEmployee/' + id);
+    return this.http.delete<IApiResponse>(
+      this.corsProxyUrl + this.apiUrl + 'DeleteEmployee/' + id
+    );
   }
 
   saveProject(obj: Employee): Observable<IProject> {
-    return this.http.post<IProject>(this.apiUrl + 'CreateProject', obj);
+    return this.http.post<IProject>(
+      this.corsProxyUrl + this.apiUrl + 'CreateProject',
+      obj
+    );
   }
 
   updateProject(obj: IProject): Observable<IProject> {
     return this.http.put<IProject>(
-      this.apiUrl + 'UpdateProject/' + obj.projectId,
+      this.corsProxyUrl + this.apiUrl + 'UpdateProject/' + obj.projectId,
       obj
     );
   }
 
   getAllProjects(): Observable<IProject[]> {
-    return this.http.get<IProject[]>(this.apiUrl + 'GetAllProjects');
+    return this.http.get<IProject[]>(
+      this.corsProxyUrl + this.apiUrl + 'GetAllProjects'
+    );
   }
 
   getProjectById(id: number): Observable<IProject> {
-    return this.http.get<IProject>(this.apiUrl + 'GetProject/' + id);
+    return this.http.get<IProject>(
+      this.corsProxyUrl + this.apiUrl + 'GetProject/' + id
+    );
   }
 
   getProjectEmp(): Observable<IProjectEmployee[]> {
     return this.http.get<IProjectEmployee[]>(
-      this.apiUrl + 'GetAllProjectEmployees'
+      this.corsProxyUrl + this.apiUrl + 'GetAllProjectEmployees'
     );
   }
 
   saveProjectEmp(obj: IProjectEmployee): Observable<IProject> {
-    return this.http.post<IProject>(this.apiUrl + 'CreateProjectEmployee', obj);
+    return this.http.post<IProject>(
+      this.corsProxyUrl + this.apiUrl + 'CreateProjectEmployee',
+      obj
+    );
   }
 
   updateProjectEmp(obj: IProjectEmployee): Observable<IProjectEmployee> {
     return this.http.put<IProjectEmployee>(
-      this.apiUrl + 'UpdateProjectEmployee/' + obj.empProjectId,
+      this.corsProxyUrl +
+        this.apiUrl +
+        'UpdateProjectEmployee/' +
+        obj.empProjectId,
       obj
     );
   }
 
   getDashboardData(): Observable<any> {
-    return this.http.get<any>(this.apiUrl + 'GetDashboard');
+    return this.http.get<any>(this.corsProxyUrl + this.apiUrl + 'GetDashboard');
   }
 }
