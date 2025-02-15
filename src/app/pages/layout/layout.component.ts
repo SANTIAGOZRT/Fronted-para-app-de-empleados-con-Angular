2dfed7a04b21a3b533677a1ef2dbceda8c69a20e
Arnob Mahmud <arnob_t78@yahoo.com>
2025-02-15 14:48:15 +0600

    update layout and employee component

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -4,15 +4,14 @@ import { Router, RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrl: './layout.component.css',
 })
 export class LayoutComponent {
-  
   router = inject(Router);
 
-  logooff() {
-    this.router.navigateByUrl('login')
+  logOff() {
+    this.router.navigateByUrl('login');
   }
 }
